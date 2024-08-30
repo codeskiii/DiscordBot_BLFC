a85038e1703066baccebe9fb74a6c6d09446458d
unknown <piotrpawelec0000@gmail.com>
2024-08-31 00:47:51 +0200

    rolling better

--- commands/utility/rolling.js
+++ commands/utility/rolling.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js'),
+const { ButtonBuilder, ActionRowBuilder, ButtonStyle, SlashCommandBuilder, Component  } = require('discord.js'),
 	fs = require('node:fs'),
 	path = require('path');
 
@@ -19,6 +19,41 @@ module.exports = {
 		let result = randomChoice(text);
 		let result_text = `WOW YOU GOT ${result}`;
 
-		await interaction.reply(result_text);
+		const rollAgain = new ButtonBuilder()
+			.setCustomId('ragain')
+			.setLabel('Roll again?')
+			.setStyle(ButtonStyle.Primary);
+
+		const nah = new ButtonBuilder()
+			.setCustomId('rnah')
+			.setLabel('nah')
+			.setStyle(ButtonStyle.Danger);
+
+		const row = new ActionRowBuilder()
+			.addComponents(rollAgain, nah);
+
+		await interaction.reply({
+			content: result_text,
+			components: [row],
+		});
+
+		const collectorFilter = i => i.user.id === interaction.user.id;
+
+		try {
+			const confirmation = await response.awaitMessageComponent({ filter: collectorFilter, time: 60_000 });
+			if (confirmation.customId === 'ragain') {
+				let result = randomChoice(text);
+				let result_text = `WOW YOU GOT ${result}`;
+				await interaction.editReply(result_text);
+			}
+			else if (confirmation.customId === 'rnah') {
+                await interaction.editReply({ content: 'Alright, no worries!', components: [] });
+            } else {
+                await interaction.editReply({ content: 'Invalid response, cancelling', components: [] });
+            }
+		} catch (e) {
+			await interaction.editReply({ content: 'Confirmation not received within 1 minute, cancelling', components: [] });
+		}
+
 	},
 };
---
+++ readme.md
@@ -0,0 +1,4 @@
+bot na server discord
+sklejka z docsow lol
+nic ciekawego i wgl
+mozna skipnac
