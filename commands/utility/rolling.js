a6c616c752c290e814a46b93a539b9b739ea6ddb
unknown <piotrpawelec0000@gmail.com>
2024-08-31 00:11:41 +0200

    rolling added

--- commands/utility/rolling.js
+++ commands/utility/rolling.js
@@ -1,10 +1,24 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js'),
+	fs = require('node:fs'),
+	path = require('path');
+
+function randomChoice(arr) {
+	return arr[Math.floor(Math.random() * arr.length)];
+}
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('e4rzeluck')
 		.setDescription('MYSTERIOUS FRUIT'),
 	async execute(interaction) {
-		await interaction.reply('WOW KITSUNE CONGRATULATIONS  !');
+		const filePath = path.join(__dirname, '../data/rolling.txt');
+
+		let text = fs.readFileSync(filePath, 'utf8');
+		text = text.split('\n');
+
+		let result = randomChoice(text);
+		let result_text = `WOW YOU GOT ${result}`;
+
+		await interaction.reply(result_text);
 	},
 };
